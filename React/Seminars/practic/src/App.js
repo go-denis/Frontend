121c127c09faa71b4c91bd2f243d0b1251dd5c55
Denis <go-denis@mail.ru>
2024-11-24 15:29:24 +0100

    Seminar 2 Comments

--- React/Seminars/practic/src/App.js
+++ React/Seminars/practic/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Message from "./components/Message";
+import CommentsList from "./components/CommentsList";
 
 function App() {
   return (
@@ -8,6 +9,10 @@ function App() {
         <h1>My React App</h1>
         <Message text="Hello, this is your first message!" />
         <Message text="This is another message!" />
+          <div className="App">
+              <h1>Мой список комментариев</h1>
+              <CommentsList />
+          </div>
       </div>
   );
 }
